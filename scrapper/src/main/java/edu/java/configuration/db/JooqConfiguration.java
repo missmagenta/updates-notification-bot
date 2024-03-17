1e73eac34765b01873e6b95f7c28d17fbbf7e940
missmagenta <sam126863@gmail.com>
2024-03-17 03:12:44 +0300

    jooq config added

--- scrapper/src/main/java/edu/java/configuration/db/JooqConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/db/JooqConfiguration.java
@@ -1,7 +1,34 @@
 package edu.java.configuration.db;
 
+import edu.java.domain.dao.jdbc.JdbcChatDao;
+import edu.java.domain.dao.jdbc.JdbcChatLinkDao;
+import edu.java.domain.dao.jdbc.JdbcLinkDao;
+import edu.java.domain.dao.jooq.JooqChatDao;
+import edu.java.domain.dao.jooq.JooqChatLinkDao;
+import edu.java.domain.dao.jooq.JooqLinkDao;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class JooqConfiguration {
+    private final DSLContext dslContext;
+
+    @Bean
+    JooqLinkDao jdbcLinkDao() {
+        return new JooqLinkDao(dslContext);
+    }
+
+    @Bean
+    JooqChatDao jdbcChatDao() {
+        return new JooqChatDao(dslContext);
+    }
+
+    @Bean
+    JooqChatLinkDao jdbcChatLinkDao() {
+        return new JooqChatLinkDao(dslContext);
+    }
 }
