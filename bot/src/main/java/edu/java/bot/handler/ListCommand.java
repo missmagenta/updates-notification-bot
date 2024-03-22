496bf500e1cd3829b225ac54e034d33274cf4852
missmagenta <sam126863@gmail.com>
2024-03-22 18:01:31 +0300

    clients integration. hw6 done

--- bot/src/main/java/edu/java/bot/handler/ListCommand.java
+++ bot/src/main/java/edu/java/bot/handler/ListCommand.java
@@ -1,20 +1,29 @@
 package edu.java.bot.handler;
 
+import edu.java.bot.client.scrapper.ScrapperClient;
+import edu.java.dto.response.ListLinkResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class ListCommand implements Command {
+
+    private final ScrapperClient scrapperClient;
+
     @Override
     public SendMessage apply(Update update) {
         long chatId = update.getMessage().getChatId();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-        sendMessage.setText("List command will be implemented later.");
 
-        return sendMessage;
+        ResponseEntity<ListLinkResponse> links = scrapperClient.getTrackedLinks((int) chatId);
+
+        return SendMessage.builder()
+            .chatId(chatId)
+            .text(Objects.requireNonNull(links.getBody()).linkResponses().toString())
+            .build();
     }
 }
--- bot/src/main/java/edu/java/bot/handler/TrackCommand.java
+++ bot/src/main/java/edu/java/bot/handler/TrackCommand.java
@@ -1,20 +1,28 @@
 package edu.java.bot.handler;
 
+import edu.java.bot.client.scrapper.ScrapperClient;
+import edu.java.dto.response.LinkResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class TrackCommand implements Command {
+    private final ScrapperClient scrapperClient;
     @Override
     public SendMessage apply(Update update) {
         long chatId = update.getMessage().getChatId();
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(chatId);
-        sendMessage.setText("Track command will be implemented later.");
+        String link = update.getMessage().getText();
 
-        return sendMessage;
+        ResponseEntity<LinkResponse> response = scrapperClient.addLinkToTrack((int) chatId, link);
+
+        return SendMessage.builder()
+            .chatId(chatId)
+            .text(Objects.requireNonNull(response.getBody()).url())
+            .build();
     }
 }
